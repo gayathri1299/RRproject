456c3a4a6aa2325ad105e6c32fba73de6673d0d8
gayathri1299 <prerepag@gmail.com>
2020-04-03 15:08:01 +0530

    healthchart and econ chart with code updated

--- code.R
+++ code.R
@@ -9,15 +9,6 @@ stormDF <- read.xlsx("repdata_data_StormData.xlsx")
 stormDT <- as.data.table(stormDF)
 
 colnames(stormDT)
-##  [1] "STATE__"    "BGN_DATE"   "BGN_TIME"   "TIME_ZONE"  "COUNTY"    
-##  [6] "COUNTYNAME" "STATE"      "EVTYPE"     "BGN_RANGE"  "BGN_AZI"   
-## [11] "BGN_LOCATI" "END_DATE"   "END_TIME"   "COUNTY_END" "COUNTYENDN"
-## [16] "END_RANGE"  "END_AZI"    "END_LOCATI" "LENGTH"     "WIDTH"     
-## [21] "F"          "MAG"        "FATALITIES" "INJURIES"   "PROPDMG"   
-## [26] "PROPDMGEXP" "CROPDMG"    "CROPDMGEXP" "WFO"        "STATEOFFIC"
-## [31] "ZONENAMES"  "LATITUDE"   "LONGITUDE"  "LATITUDE_E" "LONGITUDE_"
-## [36] "REMARKS"    "REFNUM"
-
 
 # Finding columns to remove
 cols2Remove <- colnames(stormDT[, !c("EVTYPE"
@@ -87,12 +78,6 @@ totalCostDT <- totalCostDT[order(-Total_Cost), ]
 totalCostDT <- totalCostDT[1:10, ]
 
 head(totalCostDT, 5)
-##               EVTYPE     propCost   cropCost   Total_Cost
-## 1:             FLOOD 144657709807 5661968450 150319678257
-## 2: HURRICANE/TYPHOON  69305840000 2607872800  71913712800
-## 3:           TORNADO  56947380676  414953270  57362333946
-## 4:       STORM SURGE  43323536000       5000  43323541000
-## 5:              HAIL  15735267513 3025954473  18761221986
 
 totalInjuriesDT <- stormDT[, .(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), totals = sum(FATALITIES) + sum(INJURIES)), by = .(EVTYPE)]
 
@@ -101,22 +86,11 @@ totalInjuriesDT <- totalInjuriesDT[order(-FATALITIES), ]
 totalInjuriesDT <- totalInjuriesDT[1:10, ]
 
 head(totalInjuriesDT, 5)
-##            EVTYPE FATALITIES INJURIES totals
-## 1:        TORNADO       5633    91346  96979
-## 2: EXCESSIVE HEAT       1903     6525   8428
-## 3:    FLASH FLOOD        978     1777   2755
-## 4:           HEAT        937     2100   3037
-## 5:      LIGHTNING        816     5230   6046
 
 
 bad_stuff <- melt(totalInjuriesDT, id.vars="EVTYPE", variable.name = "bad_thing")
 head(bad_stuff, 5)
-##            EVTYPE  bad_thing value
-## 1:        TORNADO FATALITIES  5633
-## 2: EXCESSIVE HEAT FATALITIES  1903
-## 3:    FLASH FLOOD FATALITIES   978
-## 4:           HEAT FATALITIES   937
-## 5:      LIGHTNING FATALITIES   816
+
 # Create chart
 healthChart <- ggplot(bad_stuff, aes(x=reorder(EVTYPE, -value), y=value))
 
@@ -136,3 +110,26 @@ healthChart = healthChart + theme(axis.text.x = element_text(angle=45, hjust=1))
 healthChart = healthChart + ggtitle("Top 10 US Killers") + theme(plot.title = element_text(hjust = 0.5))
 
 healthChart
+
+
+econ_consequences <- melt(totalCostDT, id.vars="EVTYPE", variable.name = "Damage_Type")
+head(econ_consequences, 5)
+# Create chart
+econChart <- ggplot(econ_consequences, aes(x=reorder(EVTYPE, -value), y=value))
+
+# Plot data as bar chart
+econChart = econChart + geom_bar(stat="identity", aes(fill=Damage_Type), position="dodge")
+
+# Format y-axis scale and set y-axis label
+econChart = econChart + ylab("Cost (dollars)") 
+
+# Set x-axis label
+econChart = econChart + xlab("Event Type") 
+
+# Rotate x-axis tick labels 
+econChart = econChart + theme(axis.text.x = element_text(angle=45, hjust=1))
+
+# Set chart title and center it
+econChart = econChart + ggtitle("Top 10 US Storm Events causing Economic Consequences") + theme(plot.title = element_text(hjust = 0.5))
+
+econChart
